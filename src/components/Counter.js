bb40de51ec06171fc88561a5a84e0fc1943fb4dc
Jiri Hegar <jiri.hegar@trustlab.cz>
2024-09-14 11:06:54 +0200

    zmena txtu

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,7 +6,7 @@ function Counter() {
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Citac</button>
+      <button onClick={() => setCount(count + 1)}>Citadd</button>
       <br>
       </br>
       Konecs
