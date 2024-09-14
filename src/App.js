0901727d050dc3b217cb44e09313a107c021d047
Jiri Hegar <jiri.hegar@trustlab.cz>
2024-09-14 11:02:13 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Counter from "./components/Counter"; // Importujeme Counter z nové složky
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Klikni sem pro React dokumentaci
+      </a>
+      <Counter /> {/* Přidáme komponentu Counter */}
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Citac</button>
+      <br>
+      </br>
+      Konecs
+    </div>
+  );
+}
+
+export default Counter;
